fce6e5ad183de74f45e456ba2bd4a314c34de223
NinaSleephead <kx1214@gmail.com>
2016-08-19 18:21:50 -0400

    CSS-Class-jQuery-Practice
    Practice for CSS Class in jQuery

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/*Nina Kong*/
+$(document).ready(function(){
+    var windowHeight = $( window ).height();
+    var con = $(".Content");
+    con.parentsUntil($("body")).css("height", windowHeight);
+
+    $("#add").click(function(event) {
+        $(".Content p").addClass("paraClass");
+    });
+
+    $("#toggle").click(function (event) {
+        $(".Content p").toggleClass("paraClass");
+    });
+
+    $("#remove").click(function (event) {
+        $(".Content p").removeClass("paraClass");
+    });
+
+    $("#set").click(function (event) {
+        $(".Content p").css(
+            {
+                "padding-top": "+=5px",
+                "text-decoration": "underline"
+            }
+        );
+    });
+
+});
